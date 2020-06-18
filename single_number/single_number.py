beeee8d9a50f9ba183eb0b33d9443a80a35c1291
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-06-17 21:13:44 -0500

    inititial commit

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,14 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+
+
+    # Your code here
+pass
+pass
 
 
 if __name__ == '__main__':
